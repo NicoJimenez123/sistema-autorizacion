80c694179b1b3b2ab5737eff9da288afd8ffd667
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2023-11-07 10:00:44 -0300

    Registradas rutas

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const usuarioRutas = require('./usuarioRutas')
+const modulosRutas = require('./modulosRutas')
+const permisosRutas = require('./permisosRutas')
+const recursosRutas = require('./recursosRutas')
+const rolesRutas = require('./rolesRutas')
 
 router.use('/usuarios', usuarioRutas)
+router.use('/modulos', modulosRutas)
+router.use('/permisos', permisosRutas)
+router.use('/recursos', recursosRutas)
+router.use('/roles', rolesRutas)
 
 module.exports = router
