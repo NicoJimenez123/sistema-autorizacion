ce9f2607dfebb939d3c5cad9300b619e91a3f486
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2023-11-07 20:52:34 -0300

    Añadida conexión a mongo

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+  .then(() => console.log('Conectado a Mongo'))
+  .catch(e => console.log('Error al conectarse: ', e))
 
 const rutas = require('./routes/router')
 
