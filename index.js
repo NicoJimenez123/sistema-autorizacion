260bc8ff00f3b7bceb8d033027ec8a7c390680f2
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2023-11-07 09:56:18 -0300

    Añadido router

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hola Mundo'
-    })
-})
+const rutas = require('./routes/router')
+
+app.use('/', rutas)
 
 app.listen(process.env.PORT, () => console.log('Servidor corriendo en el puerto: ', process.env.PORT))
