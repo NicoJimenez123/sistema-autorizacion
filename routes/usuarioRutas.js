f57434e2d5fff2e43790a9e330f582beb21d56bf
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2023-11-07 10:30:42 -0300

    Añadidos metodos POST, DELETE y PUT

--- routes/usuarioRutas.js
+++ routes/usuarioRutas.js
@@ -3,8 +3,28 @@ const usuarioRutas = express.Router()
 
 usuarioRutas.get('/', (req, res) => {
     res.json({
-        message: 'Hola Usuarios'
+        message: 'Hola Usuarios GET'
     })
 })
+
+usuarioRutas.post('/', (req, res) => {
+    res.json({
+        message: 'Hola Usuarios POST'
+    })
+})
+
+usuarioRutas.delete('/:id', (req, res) => {
+    res.json({
+        message: 'Hola Usuarios DELETE, id: ' + req.params.id
+    })
+})
+
+usuarioRutas.put('/:id', (req, res) => {
+    res.json({
+        message: 'Hola Usuarios PUT, id: ' + req.params.id
+    })
+})
+
+
 
 module.exports = usuarioRutas
