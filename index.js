4d65a73964841040ce7e46b8d7ac564321c34dff
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2023-11-08 18:32:23 -0300

    Añadido cors

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
+const rutas = require('./routes/router')
+
 const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING)
   .then(() => console.log('Conectado a Mongo'))
   .catch(e => console.log('Error al conectarse: ', e))
 
-const rutas = require('./routes/router')
+app.use(cors())
 app.use(express.json())
 app.use('/', rutas)
 
