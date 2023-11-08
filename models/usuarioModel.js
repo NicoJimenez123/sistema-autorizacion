644dcc5f1f56d2ded7cc4ddd5bb8ee6537b9df87
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2023-11-08 18:09:31 -0300

    Completando campo de fechas automaticamente al guardar el modelo Usuario

--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose')
 
 const usuarioSchema = mongoose.Schema({
-  usuario_id: {type: Number, required: false},
   usuario_nombre: {type: String, required: false},
   fechaCreacion: {type: Date, required: false},
   fechaModificacion: {type: Date, required: false},
   fechaEliminado: {type: Date, required: false},
+})
+
+usuarioSchema.pre('save', function (next) {
+  let now = Date.now()
+   
+  this.fechaModificacion = now
+  // Set a value for createdAt only if it is null
+  if (!this.fechaCreacion) {
+    this.fechaCreacion = now
+    this.fechaEliminado = null
+  }
+  
+  // Call the next function in the pre-save chain
+  next()    
 })
 
 module.exports = mongoose.model('Usuario', usuarioSchema)
