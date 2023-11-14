5ba71c4648648d4058b7eafef42e0e14293121b4
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2023-11-14 10:29:39 -0300

    añadida funcionalidad de login

--- routes/usuarioRutas.js
+++ routes/usuarioRutas.js
@@ -2,27 +2,56 @@ const express = require('express')
 const usuarioRutas = express.Router()
 const Usuario = require('../models/usuarioModel')
 const bcrypt = require('bcrypt')
+const auth = require('../middlewares/auth')
+const jwt = require('jsonwebtoken')
 
-usuarioRutas.get('/', (req, res) => {
-    Usuario.find().then(
-        (usuarios) => {
-            res.status(200).json(usuarios);
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
+usuarioRutas.post('/login', (req, res) => {
+    Usuario.findOne({ usuario_nombre: req.body.username }).then(
+        (usuario) => {
+          if (!usuario) {
+            return res.status(401).json({
+              error: new Error('Usuario no encontrado!')
             });
+          }
+        bcrypt.compare(req.body.password, usuario.password).then(
+        (valid) => {
+            if (!valid) {
+            return res.status(401).json({
+                error: new Error('Password incorrecto!')
+            });
+            }
+        const token = jwt.sign(
+            { tknUserId: usuario._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' });
+            res.status(200).json({
+                tknUserId: usuario._id,
+                token: token
+              });
+            }           
+          ).catch(
+            (error) => {
+              res.status(500).json({
+              error: error
+              });
+            }
+          );
+      }
+      ).catch(
+        (error) => {
+          res.status(500).json({
+            error: error
+          });
         }
-    );
+      );
 })
 
-usuarioRutas.post('/', (req, res) => {
-    bcrypt.hash(req.body.pass, 10).then(
+usuarioRutas.post('/register', (req, res) => {
+    bcrypt.hash(req.body.password, 10).then(
         (hash) => {
             const user = new Usuario({
-            usuario_nombre: req.body.usuario,
-            password: hash
+                usuario_nombre: req.body.username,
+                password: hash
             });
             user.save().then(
             () => {
@@ -41,7 +70,21 @@ usuarioRutas.post('/', (req, res) => {
         );
 })
 
-usuarioRutas.delete('/:id', (req, res) => {
+usuarioRutas.get('/', auth, (req, res) => {
+    Usuario.find().then(
+        (usuarios) => {
+            res.status(200).json(usuarios);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+})
+
+usuarioRutas.delete('/:id', auth, (req, res) => {
     Usuario.deleteOne({_id: req.params.id}).then(
         () => {
           res.status(200).json({
@@ -57,7 +100,7 @@ usuarioRutas.delete('/:id', (req, res) => {
       );
 })
 
-usuarioRutas.put('/:id', (req, res) => {
+usuarioRutas.put('/:id', auth, (req, res) => {
     let usuario = Usuario.findOne({_id: req.params.id})
     if(usuario) {
         Usuario.updateOne({_id: req.params.id}, {usuario_nombre: req.body.username}).then(
